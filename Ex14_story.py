45bda62c76e1701480a11ffbf20159406caf3cfe
Alina <alina.w.tsui@gmail.com>
2017-09-13 18:57:21 -0400

    HW 3: Exercise 14

---
+++ Ex14_story.py
@@ -0,0 +1,26 @@
+from sys import argv
+script, maker, dream, plan, future = argv
+#script = Ex14_story.py
+prompt = '> '
+
+print(f"This is a world where you can decide to be what you want, who you want to be and do what you want without restrictions." )
+
+print(f"What is your name, my child?" )
+maker = input(prompt)
+
+print(f"Welcome to this new world {maker}! Behind door one, lies a starry, starry world. \nDoor two will lead to a beautiful underwater paradise unlike anything you've seen before.")
+
+print(f"Do you wish to proceed?")
+plan = input(prompt)
+
+
+
+
+print(f"You have been specially selected for this mission...\nYou are on a path that very few have been able to access. \nAs you look back on your old life one last time, are there any regrets or decisions you wish you could undo?")
+
+dream = input(prompt)
+print(f"Fear not! The upcoming changes is meant to bring you incredible growth, happiness and wonder. It will restore your faith in humanity.")
+print(f"Which door will you choose {maker}?")
+future = input(prompt)
+print(f"You have made a wise decision!" )
+print(f"Good luck on your new journey!")
