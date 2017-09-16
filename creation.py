bbe5f9609409dfd9cef222bca81fde8e951e8a8c
ALINATSUI <ALINA.W.TSUI@GMAIL.COM>
2017-09-16 08:57:33 -0700

    Exercise 19 (own example)

---
+++ creation.py
@@ -0,0 +1,13 @@
+#step 1: define the function and arguments 
+def cheeseAndCrackers(boxesOCheese, crackers): 
+
+#step2:  
+	
+	result = ($boxesOCheese +1)
+	result1 = (crackers -.14)	
+	print(f"We are planning to have a sale on {result} boxes!")
+	print(f"Attention shoppers! We also have {result1} boxes of crackers")
+
+
+cheeseAndCrackers(5, 0)
+cheeseAndCrackers(120, 15)
