9dcb67c828ab21070bcb0d37ff1231e99894ea42
ALINATSUI <ALINA.W.TSUI@GMAIL.COM>
2017-09-16 08:54:25 -0700

    Add files via upload
    Sum of 3 numbers

---
+++ sum_exercise.py
@@ -0,0 +1,22 @@
+print(f"result for 3rd call {result}")
+
+
+#Write a function that sums 3 numbers
+
+#call the function with the following numbers
+#19, -8, 9
+#15, 9, 8
+#36, -29, 5
+
+#step1: define the function, add arguments
+
+def SumOfThree(x,y,z)
+	#step2:
+	result1 = (x+y+z)
+
+	#step3:
+	print(f"The result is: {result1})
+
+SumOfThree(19,-8,9)
+SumOfThree(15,9,8)
+SumOfThree(36,-29,5)
