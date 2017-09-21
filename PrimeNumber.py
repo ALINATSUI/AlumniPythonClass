8b6b60913f9728f38f5dde46f1872cc965a8f843
ALINATSUI <ALINA.W.TSUI@GMAIL.COM>
2017-09-20 23:56:38 -0400

    Add files via upload
    HW: write a function to determine if a number is a prime number. [For some reason, I wasn't able to get the input to print whether it was a prime number or not.]

---
+++ PrimeNumber.py
@@ -0,0 +1,18 @@
+num =int(input('Enter a number: '))
+division = (num/num)
+prime = 1
+
+def PrimeNumber(num):
+
+    #Step1: take input
+
+#Step2: if {division} = prime ==prime number
+
+    if(num>=1 and division == prime):
+        print(f"{num} is a prime number.")
+
+#Step3: else,
+    #print(f"The number you entered is a prime number")
+
+    else:
+        print(f"{num} is not a prime number.")
